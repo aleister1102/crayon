06b085c6f72f4fecb6190cdd4960674c94da4a4f
quan <marucube35@gmail.com>
2025-07-19 18:12:57 +0700

    refactor: clean up jar task configuration and remove redundant settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,17 +15,3 @@ tasks.jar {
     from(configurations.runtimeClasspath.get().filter { it.isDirectory })
     from(configurations.runtimeClasspath.get().filterNot { it.isDirectory }.map { zipTree(it) })
 }
-   archiveBaseName.set("Crayon")
-    archiveVersion.set(project.version.toString())
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    from(configurations.runtimeClasspath.get().filter { it.isDirectory })
-    from(configurations.runtimeClasspath.get().filterNot { it.isDirectory }.map { zipTree(it) })
-    
-    manifest {
-        attributes(
-            "Implementation-Title" to "Crayon",
-            "Implementation-Version" to project.version,
-            "Implementation-Vendor" to "aleister1102"
-        )
-    }
-}
